66a2c0d23be363c76cae06d3a35302e83d59716e
kckevinchenzkk <70932380+kckevinchenzkk@users.noreply.github.com>
2024-05-17 02:23:42 -0400

    update like button but some issue exist

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,12 +1,116 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Card } from "react-bootstrap";
 import Rating from "../components/Rating";
-import { Link } from "react-router-dom";
+import { Link, useParams, useHistory } from "react-router-dom";
 import Heart from "react-animated-heart";
-function Product({ product }) {
+import axios from "axios";
+
+function Product({ product, history }) {
   const [isClick, setClick] = useState(false);
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const { id } = useParams();
+  const [cartUuid, setCartUuid] = useState("");
+  const [qty, setQty] = useState(1);
+  const [likesUuid, setLikesUuid] = useState("");
   const defaultImage = process.env.PUBLIC_URL + "/images/sample.jpg";
   // console.log(product);
+
+  useEffect(() => {
+    const fetchCustomerDetails = async () => {
+      if (!userInfo) {
+        console.log("User is not logged in");
+        return;
+      }
+
+      try {
+        const config = {
+          headers: {
+            Authorization: `JWT ${userInfo.accessToken}`,
+          },
+        };
+        const { data } = await axios.get("/store/customers/", config);
+        const customerDetails = data.find(
+          (customer) => customer.user_id === userInfo.id
+        ); // Assuming userInfo.id holds the user ID
+        if (customerDetails && customerDetails.cart_id) {
+          setCartUuid(customerDetails.cart_id);
+          // console.log("Cart ID fetched:", customerDetails.cart_id);
+        } else {
+          // console.log("No cart ID found for the current user.");
+        }
+        if (customerDetails && customerDetails.likes_id) {
+          setLikesUuid(customerDetails.likes_id);
+          const wishlistData = await axios.get(
+            `/store/likes/${customerDetails.likes_id}/items/`,
+            config
+          );
+          const isProductInWishlist = wishlistData.data.some(
+            (item) => item.product.id === product.id
+          );
+
+          setClick(isProductInWishlist);
+          // console.log("Likes ID fetched:", customerDetails.likes_id);
+        } else {
+          // console.log("No likes ID found for the current user.");
+        }
+      } catch (error) {}
+    };
+
+    fetchCustomerDetails();
+  }, [userInfo, product.id]);
+
+  const addToLikesHandler = async () => {
+    if (!userInfo) {
+      history.push("/login");
+      return;
+    }
+    try {
+      let currentLikesId = likesUuid; // Assuming this state holds the current user's likes ID
+
+      // If the user does not have a likes ID, create a new likes
+      if (!currentLikesId) {
+        const config = {
+          headers: {
+            Authorization: `JWT ${userInfo.accessToken}`,
+          },
+        };
+        console.log("POSTING: /store/likes/ with ", config);
+        const { data } = await axios.post("/store/likes/", {}, config);
+        currentLikesId = data.id; // Assuming the response includes the likes ID
+        setLikesUuid(currentLikesId);
+        // Optionally, update the likesUuid state or redux store with the new likes ID
+      }
+      console.log("userInfo:", userInfo);
+      console.log("user wishlist id", likesUuid);
+      // Add the product to the likes
+      if (currentLikesId) {
+        const config = {
+          headers: {
+            Authorization: `JWT ${userInfo.accessToken}`,
+          },
+        };
+        const postData = {
+          product_id: product.id, // id from useParams()
+        };
+        console.log("posting", postData);
+        await axios.post(
+          `/store/likes/${currentLikesId}/items/`,
+          postData,
+          config
+        );
+        setClick(true); // Update the state to show the heart as clicked
+        // Redirect to cart page or show success message
+        // history.push("/wishlist");
+      }
+    } catch (error) {
+      history.push("/login");
+      console.error("Failed to add item to wishlist:", error);
+
+      // Handle error, e.g., show error message
+    }
+  };
   return (
     <Card classname="my-3 p-3 rounded">
       <div style={{ position: "relative" }}>
@@ -21,7 +125,13 @@ function Product({ product }) {
           />
         </Link>
         <div style={{ position: "absolute", top: "-30px", right: "-30px" }}>
-          <Heart isClick={isClick} onClick={() => setClick(!isClick)} />
+          <Heart
+            isClick={isClick}
+            onClick={() => {
+              setClick(!isClick);
+              addToLikesHandler();
+            }}
+          />
         </div>
       </div>
       <Card.Body>
--- frontend/src/components/screens/ProductScreen.js
+++ frontend/src/components/screens/ProductScreen.js
@@ -1,31 +1,38 @@
 import React, { useState, useEffect } from "react";
-import { Carousel } from 'react-bootstrap';
+import { Carousel } from "react-bootstrap";
 import { Link, useParams, useHistory } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Button, Card, Form } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  Form,
+} from "react-bootstrap";
 import Rating from "../Rating";
-import Loader from '../Loader';
-import Message from '../Message';
-import axios from 'axios';
+import Loader from "../Loader";
+import Message from "../Message";
+import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { listProductDetails } from "../../actions/productAction";
 import { productDetailsReducers } from "../../reducers/productReducers";
-import Product from '../Product';
+import Product from "../Product";
+import Heart from "react-animated-heart";
 
 function ProductScreen({ match, history }) {
   const { id } = useParams();
-  const [qty, setQty] = useState(1)
+  const [qty, setQty] = useState(1);
   const dispatch = useDispatch();
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
-  const [cartUuid, setCartUuid] = useState('');
-  const [likesUuid, setLikesUuid] = useState('');
+  const [cartUuid, setCartUuid] = useState("");
+  const [likesUuid, setLikesUuid] = useState("");
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
-  const defaultImage = process.env.PUBLIC_URL + '/images/sample.jpg';
+  const defaultImage = process.env.PUBLIC_URL + "/images/sample.jpg";
   const maxReviewsDisplay = 2;
-
-
-
+  const [isClickLike, setClickLike] = useState(false);
   // Function to go back to the previous page
   const goBackHandler = () => {
     history.goBack();
@@ -43,33 +50,39 @@ function ProductScreen({ match, history }) {
   useEffect(() => {
     const fetchCustomerDetails = async () => {
       if (!userInfo) {
-        console.log('User is not logged in');
+        console.log("User is not logged in");
         return;
       }
 
       try {
         const config = {
           headers: {
-            'Authorization': `JWT ${userInfo.accessToken}`,
+            Authorization: `JWT ${userInfo.accessToken}`,
           },
         };
-        const { data } = await axios.get('/store/customers/', config);
-        const customerDetails = data.find(customer => customer.user_id === userInfo.id); // Assuming userInfo.id holds the user ID
+        const { data } = await axios.get("/store/customers/", config);
+        const customerDetails = data.find(
+          (customer) => customer.user_id === userInfo.id
+        ); // Assuming userInfo.id holds the user ID
         if (customerDetails && customerDetails.cart_id) {
           setCartUuid(customerDetails.cart_id);
-          console.log('Cart ID fetched:', customerDetails.cart_id);
         } else {
-          console.log('No cart ID found for the current user.');
         }
         if (customerDetails && customerDetails.likes_id) {
           setLikesUuid(customerDetails.likes_id);
-          console.log('Likes ID fetched:', customerDetails.likes_id);
+          const wishlistData = await axios.get(
+            `/store/likes/${customerDetails.likes_id}/items/`,
+            config
+          );
+          const isProductInWishlist = wishlistData.data.some(
+            (item) => item.product.id === product.id
+          );
+
+          setClickLike(isProductInWishlist);
+          console.log(isProductInWishlist);
         } else {
-          console.log('No likes ID found for the current user.');
         }
-      } catch (error) {
-        console.error('Failed to fetch customer details:', error);
-      }
+      } catch (error) {}
     };
 
     fetchCustomerDetails();
@@ -77,7 +90,7 @@ function ProductScreen({ match, history }) {
 
   const addToCartHandler = async () => {
     if (!userInfo) {
-      history.push('/login');
+      history.push("/login");
       return;
     }
     try {
@@ -87,12 +100,12 @@ function ProductScreen({ match, history }) {
       if (!currentCartId) {
         const config = {
           headers: {
-            'Authorization': `JWT ${userInfo.accessToken}`,
+            Authorization: `JWT ${userInfo.accessToken}`,
           },
         };
-        const { data } = await axios.post('/store/carts/', {}, config);
+        const { data } = await axios.post("/store/carts/", {}, config);
         currentCartId = data.id; // Assuming the response includes the cart ID
-       
+
         // Optionally, update the cartUuid state or redux store with the new cart ID
       }
 
@@ -100,32 +113,34 @@ function ProductScreen({ match, history }) {
       if (currentCartId) {
         const config = {
           headers: {
-            'Authorization': `JWT ${userInfo.accessToken}`,
+            Authorization: `JWT ${userInfo.accessToken}`,
           },
         };
-        
+
         const postData = {
           product_id: id, // id from useParams()
           quantity: qty,
         };
 
         // console.log(currentCartId,"with id");
-        await axios.post(`/store/carts/${currentCartId}/items/`, postData, config);
+        await axios.post(
+          `/store/carts/${currentCartId}/items/`,
+          postData,
+          config
+        );
 
         // Redirect to cart page or show success message
-        history.push('/cart');
+        history.push("/cart");
       }
     } catch (error) {
-      console.error('Failed to add item to cart:', error);
+      console.error("Failed to add item to cart:", error);
       // Handle error, e.g., show error message
     }
-
-  }
-
+  };
 
   const addToLikesHandler = async () => {
     if (!userInfo) {
-      history.push('/login');
+      history.push("/login");
       return;
     }
     try {
@@ -136,13 +151,13 @@ function ProductScreen({ match, history }) {
       if (!currentLikesId) {
         const config = {
           headers: {
-            'Authorization': `JWT ${userInfo.accessToken}`,
+            Authorization: `JWT ${userInfo.accessToken}`,
           },
         };
-        console.log("POSTING: /store/likes/ with ", config)
-        const { data } = await axios.post('/store/likes/', {}, config);
+        console.log("POSTING: /store/likes/ with ", config);
+        const { data } = await axios.post("/store/likes/", {}, config);
         currentLikesId = data.id; // Assuming the response includes the likes ID
-       
+
         // Optionally, update the likesUuid state or redux store with the new likes ID
       }
 
@@ -150,7 +165,7 @@ function ProductScreen({ match, history }) {
       if (currentLikesId) {
         const config = {
           headers: {
-            'Authorization': `JWT ${userInfo.accessToken}`,
+            Authorization: `JWT ${userInfo.accessToken}`,
           },
         };
         const postData = {
@@ -158,43 +173,21 @@ function ProductScreen({ match, history }) {
           quantity: qty,
         };
 
-       
-        await axios.post(`/store/likes/${currentLikesId}/items/`, postData, config);
+        await axios.post(
+          `/store/likes/${currentLikesId}/items/`,
+          postData,
+          config
+        );
 
         // Redirect to cart page or show success message
-        history.push('/wishlist');
+        // history.push("/wishlist");
       }
     } catch (error) {
-      console.error('Failed to add item to wishlist:', error);
+      console.error("Failed to add item to wishlist:", error);
+      history.push("/login");
       // Handle error, e.g., show error message
     }
-    
-  }
-
-
-  // Testing code for loader!!!! DONT!!! DELETE!!!  
-
-  // const [isLoading, setIsLoading] = useState(false);
-
-  // useEffect(() => {
-  //     setIsLoading(true); // Start loading
-  //     const timer = setTimeout(() => {
-  //         setIsLoading(false); // End loading after 5 seconds
-  //     }, 5000); // Adjust time as needed for your test
-
-  //     return () => clearTimeout(timer); // Cleanup timeout
-  //   }, [product]);
-
-
-  //   if (isLoading) {
-  //       return <Loader />;
-  //   }
-
-  //   if (error) {
-  //       return <Message variant='danger'>{error}</Message>;
-  //   }
-
-
+  };
 
   return (
     <div>
@@ -202,12 +195,10 @@ function ProductScreen({ match, history }) {
         Go Back
       </button>
 
-
       {loading ? (
         <Loader />
-
       ) : error ? (
-        <Message variant='danger'>{error} </Message>
+        <Message variant="danger">{error} </Message>
       ) : (
         <>
           <Row>
@@ -225,13 +216,34 @@ function ProductScreen({ match, history }) {
                   ))}
                 </Carousel>
               ) : (
-                <img src={defaultImage} alt="Default product" className="img-fluid" />
+                <img
+                  src={defaultImage}
+                  alt="Default product"
+                  className="img-fluid"
+                />
               )}
             </Col>
             <Col md={3}>
               <ListGroup variant="flush">
                 <ListGroup.Item>
-                  <h3>{product.title}</h3>
+                  <div style={{ position: "relative" }}>
+                    <h3>{product.title}</h3>
+                    <div
+                      style={{
+                        position: "absolute",
+                        top: "-40px",
+                        right: "-55px",
+                      }}
+                    >
+                      <Heart
+                        isClick={isClickLike}
+                        onClick={() => {
+                          setClickLike(!isClickLike);
+                          addToLikesHandler();
+                        }}
+                      />
+                    </div>
+                  </div>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <Rating
@@ -269,9 +281,7 @@ function ProductScreen({ match, history }) {
 
                   {product.inventory > 0 && (
                     <ListGroup.Item>
-
                       <Row>
-
                         <Col>Qty</Col>
                         <Col xs="auto" className="my-1">
                           <Form.Control
@@ -286,8 +296,6 @@ function ProductScreen({ match, history }) {
                             ))}
                           </Form.Control>
                         </Col>
-
-
                       </Row>
                     </ListGroup.Item>
                   )}
@@ -302,7 +310,6 @@ function ProductScreen({ match, history }) {
                     </Button>
                   </ListGroup.Item>
 
-
                   <ListGroup.Item>
                     <Button
                       className="btn-block"
@@ -313,7 +320,6 @@ function ProductScreen({ match, history }) {
                       Add to Cart
                     </Button>
                   </ListGroup.Item>
-
                 </ListGroup>
               </Card>
             </Col>
@@ -324,15 +330,19 @@ function ProductScreen({ match, history }) {
             {product.reviews && product.reviews.length > 0 ? (
               <ListGroup variant="flush">
                 {/* limit the number of reviews displayed, in descending order of date */}
-                {product.reviews.sort((a, b) => new Date(b.date) - new Date(a.date))                  
-                  .slice(0, maxReviewsDisplay).map((review) => {
+                {product.reviews
+                  .sort((a, b) => new Date(b.date) - new Date(a.date))
+                  .slice(0, maxReviewsDisplay)
+                  .map((review) => {
                     // console.log(review);
                     return (
                       <ListGroup.Item key={review.id}>
                         {/* <strong>{review.name}</strong> */}
                         <Rating value={review.rating} />
                         <p>{review.date}</p>
-                        <p  className="review-description">{review.description}</p>
+                        <p className="review-description">
+                          {review.description}
+                        </p>
                       </ListGroup.Item>
                     );
                   })}
@@ -346,22 +356,41 @@ function ProductScreen({ match, history }) {
           <div className="mt-5">
             <h3>Similar products you may like</h3>
             <div className="horizontal-scroll-wrapper">
-            {product.recommended_products &&
-              product.recommended_products.map((recommendedProduct) => (
-                <div key={recommendedProduct.id} className="product-card m-2">
-                  {recommendedProduct.images && recommendedProduct.images.length > 0 ? (
-                    <img
-                      src={process.env.PUBLIC_URL + recommendedProduct.images[0].image}
-                      alt={recommendedProduct.title}
-                      className="img-fluid"
-                    />
-                  ) : (
-                    <img src={defaultImage} alt="Default product" className="img-fluid" />
-                  )}
+              {product.recommended_products &&
+                product.recommended_products.map((recommendedProduct) => (
+                  <div key={recommendedProduct.id} className="product-card m-2">
+                    {recommendedProduct.images &&
+                    recommendedProduct.images.length > 0 ? (
+                      <img
+                        src={
+                          process.env.PUBLIC_URL +
+                          recommendedProduct.images[0].image
+                        }
+                        alt={recommendedProduct.title}
+                        className="img-fluid"
+                      />
+                    ) : (
+                      <img
+                        src={defaultImage}
+                        alt="Default product"
+                        className="img-fluid"
+                      />
+                    )}
                     <div className="product-info">
-                      <h3 className="product-subtitle">{recommendedProduct.title}</h3>
-                      <p className="product-description">{recommendedProduct.description}</p>
-                      <Button variant="primary" onClick={() => history.push(`/products/${recommendedProduct.id}`)}>View Details</Button>
+                      <h3 className="product-subtitle">
+                        {recommendedProduct.title}
+                      </h3>
+                      <p className="product-description">
+                        {recommendedProduct.description}
+                      </p>
+                      <Button
+                        variant="primary"
+                        onClick={() =>
+                          history.push(`/products/${recommendedProduct.id}`)
+                        }
+                      >
+                        View Details
+                      </Button>
                     </div>
                   </div>
 
@@ -370,16 +399,14 @@ function ProductScreen({ match, history }) {
                   //   <Card.Body>
                   //     <Card.Title style={{ fontSize: "1rem",width: "10rem" , marginBottom: "0.5rem" , overflowWrap: "break-word"}}>
                   //       {recommendedProduct.title} </Card.Title>
-                  //     <Card.Text style={{ width: "10rem" , overflowWrap : "break-word"}}>{recommendedProduct.description.length > 15? 
+                  //     <Card.Text style={{ width: "10rem" , overflowWrap : "break-word"}}>{recommendedProduct.description.length > 15?
                   //       recommendedProduct.description.substring(0, 15) + "...": recommendedProduct.description }</Card.Text>
                   //     <Button variant="primary" onClick={() => history.push(`/products/${recommendedProduct.id}`)}>View Details</Button>
                   //   </Card.Body>
                   // </Card>
-                ))
-              }
+                ))}
             </div>
           </div>
-
         </>
       )}
     </div>
