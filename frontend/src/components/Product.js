1bafc11c0e317d8826e83cc78f284a3847f7c955
kckevinchenzkk <70932380+kckevinchenzkk@users.noreply.github.com>
2024-05-16 23:44:44 -0400

    adding a like button

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 import Rating from "../components/Rating";
-import {Link} from "react-router-dom"
-
+import { Link } from "react-router-dom";
+import Heart from "react-animated-heart";
 function Product({ product }) {
-  const defaultImage = process.env.PUBLIC_URL + '/images/sample.jpg';
-  // console.log(product); 
+  const [isClick, setClick] = useState(false);
+  const defaultImage = process.env.PUBLIC_URL + "/images/sample.jpg";
+  // console.log(product);
   return (
     <Card classname="my-3 p-3 rounded">
-      <Link to={`/products/${product.id}`}>
-      <Card.Img src={product.images && product.images.length > 0 ? product.images[0].image : defaultImage} variant="top" />
-      </Link>
-
+      <div style={{ position: "relative" }}>
+        <Link to={`/products/${product.id}`}>
+          <Card.Img
+            src={
+              product.images && product.images.length > 0
+                ? product.images[0].image
+                : defaultImage
+            }
+            variant="top"
+          />
+        </Link>
+        <div style={{ position: "absolute", top: "-30px", right: "-30px" }}>
+          <Heart isClick={isClick} onClick={() => setClick(!isClick)} />
+        </div>
+      </div>
       <Card.Body>
         <Link to={`/products/${product.id}`}>
           <Card.Title as="div">
@@ -20,20 +32,18 @@ function Product({ product }) {
         </Link>
 
         <Card.Text as="div">
-            <div className="my-3">
-                {product.average_rating} from {product.total_reviews} reviews
-            </div>
-        </Card.Text>
-        
-        <Card.Text as="h3">
-          {product.unit_price}
+          <div className="my-3">
+            {product.average_rating} from {product.total_reviews} reviews
+          </div>
         </Card.Text>
 
+        <Card.Text as="h3">{product.unit_price}</Card.Text>
+
         <Rating
-                value={product.average_rating}
-                text={` ${product.total_reviews} reviews`}
-                color={"#f8e825"}
-              />
+          value={product.average_rating}
+          text={` ${product.total_reviews} reviews`}
+          color={"#f8e825"}
+        />
       </Card.Body>
     </Card>
   );
