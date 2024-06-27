3eb5647e5ae3c6d282103ba5e60db1729a0662b7
Zhengkang Chen <70932380+kckevinchenzkk@users.noreply.github.com>
2024-06-27 18:16:44 -0400

    meet our team half way

--- frontend/src/components/MindmapNode.js
+++ frontend/src/components/MindmapNode.js
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import { Image } from "react-bootstrap";
-import "./MindmapNode.css"; // Make sure to create this CSS file
+import "./MindmapNode.css";
 
-const MindmapNode = ({ person, onClick }) => {
-    return (
-      <div className="mindmap-node" onClick={() => onClick(person)}>
-        <Image
-          src={person.headshot}
-          alt={person.name}
-          roundedCircle
-          className="headshot"
-        />
-        {person.showInfo && (
-          <div className="info">
-            <h3>{person.name}</h3>
-            <p>{person.role}</p>
-          </div>
-        )}
-      </div>
-    );
-  };
-  
-  export default MindmapNode;
+const MindmapNode = ({ person, onClick, style }) => {
+  return (
+    <div className="mindmap-node" onClick={() => onClick(person)} style={style}>
+      <Image
+        src={person.headshot}
+        alt={person.name}
+        roundedCircle
+        className="headshot"
+      />
+      {person.showInfo && (
+        <div className="info">
+          <h3>{person.name}</h3>
+          <p>{person.role}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MindmapNode;
